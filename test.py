3452f55eba075bab03c54f62adbb2444eb18c500
Mike <lvlichaelly@gmail.com>
2015-09-25 00:10:47 -0700

    learning python

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-print("hello") print("moto")
+'''
+print("hello")
 print("hi")
+
+name = input("whats ur name?")
+print("hi" + name)
+
+name = True
+
+if name == True:
+    print("true")
+else:
+    print(name)
+
+'''
+i = 0
+while i < 10 :
+    print(i)
+    i += 1
+
+user_input = input("imput a number")
+# user_input = int (user_input)
+
+print(type(user_input))
